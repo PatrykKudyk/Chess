7f693e2c3bcc534ce6100a91e2e8ddfb14113de4
Papryk <gustaw5022@gmail.com>
2020-10-07 10:28:24 +0200

    done layout

---
+++ Chess/app/src/main/java/com/partos/chess/logic/BoardHelper.kt
@@ -0,0 +1,11 @@
+package com.partos.chess.logic
+
+import android.view.View
+import android.widget.ImageView
+
+class BoardHelper {
+
+//    fun createBoardArray(rootView: View): Array<Array<ImageView>> {
+//
+//    }
+}
